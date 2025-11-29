e1c3be63752c73f359a7ed148f24b0713406082c
matheussaldanha22 <matheus.amaral@sptech.school>
2025-11-28 21:13:20 -0300

    roteamento de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
+import Login from "./pages/Login/Login.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Roteamento de telas, aqui chama a função da tela e o path é o caminho que vai ser acessado */}
         <Route path="/" element={<Login />} />
       </Routes>
 
