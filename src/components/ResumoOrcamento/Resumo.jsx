167a567c8d97e3ebf6ea3edc48c914c9e64402a4
henriquedbarros <henrique_ab8@Hotmail.com>
2026-02-16 15:41:26 -0300

    Parte superior dinamico de acordo com a pagina feito

--- src/components/ResumoOrcamento/Resumo.jsx
+++ src/components/ResumoOrcamento/Resumo.jsx
@@ -3,45 +3,56 @@ import { useState, useEffect } from "react";
 import { buscarServicos, buscarItens } from "../../service/api";
 import Servicos from "./Servicos";
 import Itens from "./Itens";
+import { useLocation } from "react-router-dom";
 
 function Resumo() {
     const [abaAtiva, setAbaAtiva] = useState("servicos");
+    const location = useLocation();
+    const [paginaAtual, setPaginaAtual] = useState("analise");
     const [dados, setDados] = useState([]);
 
     useEffect(() => {
-    const carregarDados = async () => {
-        try {
-            if (abaAtiva === "servicos") {
-                const res = await buscarServicos();
+        const carregarDados = async () => {
+            try {
+                if (abaAtiva === "servicos") {
+                    const res = await buscarServicos();
 
-                const lista = Array.isArray(res.data)
-                    ? res.data
-                    : res.data.content ?? res.data.servicos ?? [];
+                    const lista = Array.isArray(res.data)
+                        ? res.data
+                        : res.data.content ?? res.data.servicos ?? [];
 
-                setDados(lista);
+                    setDados(lista);
 
-            } else {
-                const res = await buscarItens();
+                } else {
+                    const res = await buscarItens();
 
-                const lista = Array.isArray(res.data)
-                    ? res.data
-                    : res.data.content ?? res.data.itens ?? [];
+                    const lista = Array.isArray(res.data)
+                        ? res.data
+                        : res.data.content ?? res.data.itens ?? [];
 
-                setDados(lista);
+                    setDados(lista);
+                }
+            } catch (error) {
+                console.error("Erro ao buscar dados:", error);
+                setDados([]);
             }
-        } catch (error) {
-            console.error("Erro ao buscar dados:", error);
-            setDados([]);
-        }
-    };
+        };
 
-    carregarDados();
-}, [abaAtiva]);
+        carregarDados();
+    }, [abaAtiva]);
     return (
         <div className="resumo-container">
+            {/* Progesso */}
+            {paginaAtual === "finalizado" || paginaAtual === "producao" ?
+                <div>Teste</div>
+                : null
+            }
+
+
             {/* Parte superior do resumo */}
             <div className="bar-menu">
-                <div className="bar-options">
+                <div className={`bar-options ${
+                    paginaAtual === "analise" ? "full" : ""}`}>
                     <button
                         className={`buttons ${abaAtiva === "servicos" ? "selecionado" : ""}`}
                         onClick={() => setAbaAtiva("servicos")}
@@ -57,23 +68,34 @@ function Resumo() {
                     </button>
                 </div>
 
-                <div className="options-action">
-                    <button className="add">
-                        {abaAtiva === "servicos"
-                            ? "Adicionar Serviço"
-                            : "Adicionar Item"}
-                    </button>
-                </div>
+                {paginaAtual != "analise" ?
+                    <div className="options-action">
+                        {
+                            paginaAtual === "orcamento" ?
+                                <button className="add">
+                                    {abaAtiva === "servicos"
+                                        ? "Adicionar Serviço"
+                                        : "Adicionar Item"}
+                                </button>
+                                :
+                                <button className="imprimir">
+                                    Imprimir
+                                </button>
+                        }
+                    </div>
+                    : null
+                    // Aqui em cima
+                }
             </div>
 
             {/* Parte dos tickets */}
 
             <div>
                 {abaAtiva === "servicos" ? (
-                <Servicos dados={dados} />
-            ) : (
-                <Itens dados={dados} />
-            )}
+                    <Servicos dados={dados} />
+                ) : (
+                    <Itens dados={dados} />
+                )}
             </div>
         </div>
     );
