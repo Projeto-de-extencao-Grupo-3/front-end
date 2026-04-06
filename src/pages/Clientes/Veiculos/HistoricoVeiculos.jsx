cbe20114b4c274c71c3a67f41aa9dc738f462258
LucasDev-end <lucas.marcolino@sptech.school>
2026-04-05 23:10:07 -0300

    Integração das telas de veiculos

--- src/pages/Clientes/Veiculos/HistoricoVeiculos.jsx
+++ src/pages/Clientes/Veiculos/HistoricoVeiculos.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Layout from "../../../components/Layout/Layout.jsx";
 import TabelaVeiculos from "../../../components/Layout/TabelaVeiculos.jsx";
+<<<<<<< Updated upstream
 
 function HistoricoVeiculos() {
     return (
@@ -13,6 +14,24 @@ function HistoricoVeiculos() {
                 <Link to="/clientes" className="btn btn-dark px-4">
                     Voltar
                 </Link>
+=======
+import "./HistoricoVeiculos.css";
+
+function HistoricoVeiculos() {
+    
+    return (
+        <Layout ativo={"clientes"}>
+            <div className="header-clientes">
+                <div>
+                    <h1>Histórico de Veículos</h1>
+                    <p>Controle completo de veículos e serviços realizados</p>
+                </div>
+                <div className="d-flex gap-3 align-items-center">
+                    <Link to="/clientes" className="add_client btn btn-dark d-flex justify-content-center align-items-center h-100 w-100">
+                        Voltar
+                    </Link>
+                </div>
+>>>>>>> Stashed changes
             </div>
             <TabelaVeiculos />
         </Layout>
